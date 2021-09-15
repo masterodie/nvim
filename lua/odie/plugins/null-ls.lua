f480cd92c5b461077daec8211c27c5195b383e22
Patrick Neff <odie86@gmail.com>
2021-09-15 15:55:43 +0200

    Use black as default python formatter

--- lua/odie/plugins/null-ls.lua
+++ lua/odie/plugins/null-ls.lua
@@ -9,7 +9,7 @@ null_ls.config({
 		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.diagnostics.shellcheck,
 		--null_ls.builtins.diagnostics.luacheck,
-		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.eslint,
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.stylua,
