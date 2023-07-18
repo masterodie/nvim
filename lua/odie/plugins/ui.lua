2dd07fb6e3d2d7091d4a04249d0b39bc5d669baa
Patrick Neff <odie86@gmail.com>
2023-07-18 09:02:24 +0200

    Add transparency plugin

--- lua/odie/plugins/ui.lua
+++ lua/odie/plugins/ui.lua
@@ -17,11 +17,11 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
     },
     dependencies = {
@@ -144,15 +144,15 @@ return {
         winbar = {
           lualine_x = {
             { "diagnostics" },
-            { "filename", path = 1 },
-            { "filetype", icon_only = true },
+            { "filename",   path = 1 },
+            { "filetype",   icon_only = true },
           },
         },
         inactive_winbar = {
           lualine_x = {
             { "diagnostics" },
-            { "filename", path = 1 },
-            { "filetype", icon_only = true },
+            { "filename",   path = 1 },
+            { "filetype",   icon_only = true },
           },
         },
         extensions = { "fugitive", "lazy", "man", "neo-tree", "nvim-dap-ui", "quickfix" },
@@ -200,4 +200,9 @@ return {
       }
     end,
   },
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {},
+    event = 'ColorScheme',
+  }
 }
