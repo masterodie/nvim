81c69679ce5125353182758ada6b86b087f090fa
Patrick Neff <odie86@gmail.com>
2023-10-04 02:25:08 +0200

    Move augroup out of lsp plugin config.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,29 +3,6 @@ local nvim_lsp = require("lspconfig")
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-local lsp_attach_augroup = vim.api.nvim_create_augroup("lsp_attach", { clear = true })
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = lsp_attach_augroup,
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Defninition" })
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
-        vim.keymap.set("n", "gtd", vim.lsp.buf.type_definition, { desc = "Show Type Definition" })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto References" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.cmd.CodeActionMenu, { desc = "Code action" })
-        vim.keymap.set("n", "<leader>fm", function()
-            vim.lsp.buf.format({ async = true })
-        end, { desc = "Format Code" })
-    end,
-})
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
